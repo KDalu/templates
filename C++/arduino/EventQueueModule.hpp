5e7564288229568359ca4c85ba1bc48bb383dc6b
KDalu <71458929+KDalu@users.noreply.github.com>
2024-06-27 21:36:50 -0400

    Added license banner

--- C++/arduino/EventQueueModule.hpp
+++ C++/arduino/EventQueueModule.hpp
@@ -1,3 +1,28 @@
+/***********************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2024 KDalu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ***********************************************************************************/
+
 #ifndef EVENT_QUEUE_MODULE_HPP
 #define EVENT_QUEUE_MODULE_HPP
 
@@ -5,17 +30,17 @@
 
 /***********************************************************************************
  * Definitions and Macros
- ***********************************************************************************/
+ **********************************************************************************/
 #define EVENT_QUEUE_LENGTH 1024
 
 /***********************************************************************************
  * Typedefs
- ***********************************************************************************/
+ **********************************************************************************/
 
 
 /***********************************************************************************
  * Enumerations and Structures
- ***********************************************************************************/
+ **********************************************************************************/
 enum EventId_t
 {
     TASK_FIRST_EVENT = 0,
@@ -52,7 +77,7 @@ struct EventQueueWs_t
 
 /***********************************************************************************
  * Function Declarations
- ***********************************************************************************/
+ **********************************************************************************/
 void initEventQueue();
 int sendEvent(EventId_t EventId, TaskId_t TaskId, TimerId_t TimerId = 128);
 int receiveEvent(EventInfo_t* pEvent);
--- C++/arduino/Task.hpp
+++ C++/arduino/Task.hpp
@@ -1,3 +1,28 @@
+/***********************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2024 KDalu
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ ***********************************************************************************/
+
 #ifndef TASK_HPP
 #define TASK_HPP
 
@@ -5,26 +30,26 @@
 
 /***********************************************************************************
  * Definitions and Macros
- ***********************************************************************************/
+ **********************************************************************************/
 
 
 /***********************************************************************************
  * Typedefs
- ***********************************************************************************/
+ **********************************************************************************/
 
 
 /***********************************************************************************
  * Enumerations and Structures
- ***********************************************************************************/
+ **********************************************************************************/
 
 
 /***********************************************************************************
  * Class Definition
- ***********************************************************************************/
+ **********************************************************************************/
 class Task
 {
 private:
-    enum TaskStates
+    enum TaskStates_t
     {
         TASK_FIRST_STATE = 0,
         TASK_SECOND_STATE,
@@ -34,13 +59,14 @@ private:
     EventInfo_t* TaskFirstState(EventInfo_t* pEvent);
     EventInfo_t* TaskSecondState(EventInfo_t* pEvent);
 
-    typedef EventInfo_t* (Task::*TaskStateMachine)(EventInfo_t* pEvent);
+    typedef EventInfo_t* (Task::*TaskStateMachine_t)(EventInfo_t* pEvent);
 
-    TaskStateMachine TaskFsm[NUM_TASK_STATES] = {&Task::TaskFirstState,
+    TaskStateMachine_t TaskFsm[NUM_TASK_STATES] = {&Task::TaskFirstState,
                                                  &Task::TaskSecondState
                                                 };
 
     // Private variables
+    TaskStates_t currentState;
 
     // Private functions
 
@@ -50,5 +76,7 @@ public:
 
     // Public functions
 };
+
+extern Task TaskWs;
 
 #endif
